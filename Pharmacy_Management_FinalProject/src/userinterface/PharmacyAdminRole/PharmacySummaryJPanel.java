8cda0761bbb6b06a69ef823d7cb3ba04e1133917
ruchi-kapadiwala-nu <ruchikapadiwala@Ruchis-MacBook-Air.local>
2022-12-08 13:16:53 -0500

    Pharmacy order history functionality executed

--- Pharmacy_Management_FinalProject/src/userinterface/PharmacyAdminRole/PharmacySummaryJPanel.java
+++ Pharmacy_Management_FinalProject/src/userinterface/PharmacyAdminRole/PharmacySummaryJPanel.java
@@ -4,17 +4,40 @@
  */
 package userinterface.PharmacyAdminRole;
 
+import Business.EcoSystem;
+import Business.Pharmacy.Pharmacy;
+import Business.Supplier.Supplier;
+import Business.SupplierMedicineItem.SupplierMedicineItem;
+import Business.SupplierOrders.SupplierOrders;
+import Business.UserAccount.UserAccount;
+import java.awt.CardLayout;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author ruchikapadiwala
  */
 public class PharmacySummaryJPanel extends javax.swing.JPanel {
+private JPanel userProcessContainer;
 
+    private UserAccount user;
+    private Pharmacy pharmacy;
+    private EcoSystem system;
     /**
      * Creates new form PharmacySummaryJPanel
      */
-    public PharmacySummaryJPanel() {
+    public PharmacySummaryJPanel(JPanel userProcessContainer, UserAccount account, EcoSystem system, Pharmacy pharmacy) {
         initComponents();
+        this.userProcessContainer = userProcessContainer;     
+        this.user = account;
+        this.system = system;
+        System.out.println(system.getPharmacyDirectory().getPharmacyList());
+        this.pharmacy = pharmacy;
+        enterpriseLabel.setText(this.pharmacy + "'s orders");
+        populateRequestTable();
     }
 
     /**
@@ -104,16 +127,47 @@ public class PharmacySummaryJPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void requestTestJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_requestTestJButtonActionPerformed
-
+        int selectedRow = workRequestJTable.getSelectedRow();
+        if (selectedRow >= 0)
+        {
+            if((workRequestJTable.getValueAt(selectedRow, 1)) == null)
+            {
+                JOptionPane.showMessageDialog(null,"Order is not live anymore!", "Warning", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            else
+            {
+                SupplierOrders so = (SupplierOrders) workRequestJTable.getValueAt(selectedRow, 0);
+                PharmacyRequestJPanel fs = new PharmacyRequestJPanel(userProcessContainer, so);
+                userProcessContainer.add("SysAdminManageEmployees", fs);
+                CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+                layout.next(userProcessContainer);
+            }
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Please select a row!", "Warning", JOptionPane.WARNING_MESSAGE);
+            return;
+        }
     }//GEN-LAST:event_requestTestJButtonActionPerformed
 
     private void refreshTestJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_refreshTestJButtonActionPerformed
-        
+        if(workRequestJTable.getRowCount() > 0)
+        {
+            populateRequestTable();
+            JOptionPane.showMessageDialog(null, "Table refreshed!");
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"Nothing to refresh!", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
     }//GEN-LAST:event_refreshTestJButtonActionPerformed
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
         // TODO add your handling code here:
-        
+        userProcessContainer.remove(this);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.previous(userProcessContainer);
     }//GEN-LAST:event_btnBackActionPerformed
 
 
@@ -125,4 +179,48 @@ public class PharmacySummaryJPanel extends javax.swing.JPanel {
     private javax.swing.JButton requestTestJButton;
     private javax.swing.JTable workRequestJTable;
     // End of variables declaration//GEN-END:variables
+    public void populateRequestTable() {
+        if(pharmacy.getPastSupplierOrderList() != null)
+        {
+            DefaultTableModel dtm = (DefaultTableModel)workRequestJTable.getModel();
+            dtm.setRowCount(0);
+            
+            refreshTestJButton.setEnabled(true);
+            requestTestJButton.setEnabled(true);
+            int count = 1;
+            for(SupplierOrders so : pharmacy.getPastSupplierOrderList())
+            {
+                if(system.getSupplierDirectory().getSupplierList() != null)
+                {
+                    Object[] row = new Object[dtm.getColumnCount()];
+                    row[0] = so;
+                    for(Supplier s : system.getSupplierDirectory().getSupplierList())
+                    {
+                        for(SupplierOrders or : s.getSupplierOrderDirectory().getSupplierOrderList())
+                        {
+                          if(so.equals(or))
+                          {
+                              row[1] = s;
+                          }
+                        }
+                    }
+                    row[2] = so.getTotalAmount();
+                    row[3] = so.getMessage();
+                    row[4] = so.getSupplierDeliveryMan();
+                    if(so.isStatus())
+                    {
+                        row[5] = "Yes";
+                    }
+                    else
+                    {
+                        row[5] = "No";
+                    }
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    row[6] = so.getOrderDate().format(formatter);
+                    dtm.addRow(row);
+                    count++;
+                }
+            }
+        }
+    }
 }
